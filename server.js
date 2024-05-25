71a7818b114662009795d0b2b35d2f50d376bf64
MuhammadAmeerHamzakhan <pakhamza99@gmail.com>
2024-05-26 01:59:19 +0500

    first commit

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ let fs = require("fs");
 
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
+    fs.writeFile('example.txt', "Hello World", function(err){
+        console.log(err)
+    })
     fs.readFile('example.txt', (err, data) => {
-        if (err) {
-            console.log(err);
-            res.end('Err');
-        } else {
-            console.log(data); 
-            res.end(data);
-        }
+        res.end=(data.toString())
+    //     if (err) {
+    //         console.log(err);
+    //         res.end('Err');
+    //     } else {
+    //         console.log(data); 
+            // res.end(data);
+    //     }
     });
 }).listen(5000, () => {
     console.log("Server is running on port 5000");
